9070ff4dac1ce90779726b1e928a4bbc25661910
George Brooke <George Brooke>
2017-03-13 20:03:13 +0000

    Added documentation comments

--- main.ts
+++ main.ts
@@ -8,9 +8,21 @@ namespace keyboard {
     }
 
     // If you're wondering why these symbols, they're ASCII range 33-126, as supported in the default micro:bit font (see https://github.com/lancaster-university/microbit-dal/blob/master/inc/core/MicroBitFont.h)
-    export const letters: string[] =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-    export const numbers: string[] =['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-    export const symbols: string[] =['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '@', '[', ']', '^', '_', '`', '\\', '{', '}', '|', '~'];
+    /**
+     * Array of capital and lowercase letters
+     */
+    export const letters: string[] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    /**
+     * Array of numbers
+     */
+    export const numbers: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    /**
+     * Array of symbols
+     */
+    export const symbols: string[] = ['!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '?', '@', '[', ']', '^', '_', '`', '\\', '{', '}', '|', '~'];
+    /**
+     * Combined array of numbers, letters, and symbols
+     */
     export const everything: string[] = joinArray(joinArray(letters, numbers), symbols);
 
      /**
@@ -35,7 +47,6 @@ namespace keyboard {
                 } else {
                     basic.pause(100);
                     buttonsHeld += 100;
-                    //basic.showNumber(buttonsHeld / 100, 50);
                 }
             } else if (input.buttonIsPressed(Button.A)) {
                 if (currentLetter <= 0) {
